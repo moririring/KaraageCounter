aeb274c4a83fbf29a2046cdcaa5d29ff9c165405
moririring <moriri.ring10@gmail.com>
2015-01-04 03:12:46 +0900

    認証を追加

--- KaraageCounter/App_Start/Startup.Auth.cs
+++ KaraageCounter/App_Start/Startup.Auth.cs
@@ -46,23 +46,23 @@ namespace KaraageCounter
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // 次の行のコメントを解除して、サード パーティのログイン プロバイダーを使用したログインを有効にします
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            app.UseMicrosoftAccountAuthentication(
+                clientId: "000000004013AEE9",
+                clientSecret: "Gf4p-ULT7b8kjanlLEcDAomWaUJc84Od");
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            app.UseTwitterAuthentication(
+               consumerKey: "ceOIpL3zYbQG4O6RMVNE9iIqR",
+               consumerSecret: "45TbMEA2HvTzFdfsXCjUjh5xpQHef3G2j1rn4wknB3Q0gZhXZJ");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "398349567005614",
+               appSecret: "063f412b9d8b4107af5940654cd07259");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "242226326873-1hg765n1aibe6s6qh9qaajlpegjtj06r.apps.googleusercontent.com",
+                ClientSecret = "puZLEUJi0lJjc0v5R21h5erK"
+            });
         }
     }
 }
