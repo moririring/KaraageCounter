dbafb56450f34155553ac24fecae47344963acd0
moririring <moriri.ring10@gmail.com>
2015-01-17 12:55:16 +0900

    もろもろ細かいのをアップ

--- KaraageCounter/App_Start/AccessKey.cs
+++ KaraageCounter/App_Start/AccessKey.cs
@@ -7,13 +7,13 @@ namespace KaraageCounter.Controllers
 {
     public class AccessKey
     {
-        public const string MicrosoftclientId = "";
-        public const string MicrosoftclientSecret = "";
-        public const string TwitterconsumerKey = "";
-        public const string TwitterconsumerSecret = "";
-        public const string FacebookappId = "";
-        public const string FacebookappSecret = "";
-        public const string GoogleClientId = "";
-        public const string GoogleClientSecret = "";
+        public const string MicrosoftclientId = "000000004013AEE9";
+        public const string MicrosoftclientSecret = "Gf4p-ULT7b8kjanlLEcDAomWaUJc84Od";
+        public const string TwitterconsumerKey = "ceOIpL3zYbQG4O6RMVNE9iIqR";
+        public const string TwitterconsumerSecret = "45TbMEA2HvTzFdfsXCjUjh5xpQHef3G2j1rn4wknB3Q0gZhXZJ";
+        public const string FacebookappId = "398349567005614";
+        public const string FacebookappSecret = "063f412b9d8b4107af5940654cd07259";
+        public const string GoogleClientId = "242226326873-1hg765n1aibe6s6qh9qaajlpegjtj06r.apps.googleusercontent.com";
+        public const string GoogleClientSecret = "puZLEUJi0lJjc0v5R21h5erK";
     }
 }
---
+++ KaraageCounterTest/Controllers/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaraageCounterTest.Controllers
+{
+    internal class Class1
+    {
+        public class Foo
+        {
+            public String Bar { get; set; }
+        }
+
+        public void test()
+        {
+            Method1();
+            Method2();
+        }
+
+        public static void Method1()
+        {
+            Foo foofoo = null;
+            try
+            {
+                foofoo = new Foo();
+                foofoo.Bar = Throw();
+            }
+            catch
+            {
+                Console.WriteLine(foofoo == null ? "nullです" : "nullじゃないです");
+            }
+
+        }
+
+        public static void Method2()
+        {
+            Foo foofoo = null;
+            try
+            {
+                foofoo = new Foo()
+                {
+                    Bar = Throw()
+                };
+            }
+            catch
+            {
+                Console.WriteLine(foofoo == null ? "nullです" : "nullじゃないです");
+            }
+        }
+
+        public static string Throw()
+        {
+            throw new Exception();
+        }
+    }
+
+}
+
